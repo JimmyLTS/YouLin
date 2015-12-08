f03263c325bf4087877414eb0c66bb64d550e02d
Jimmy <Jimmy@jimmydemacbook-pro.local>
2015-12-08 16:07:46 +0800

    Initial Commit

---
+++ YouLin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YouLin
+//
+//  Created by Jimmy on 15/12/8.
+//  Copyright © 2015年 Jimmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YouLin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YouLin
+//
+//  Created by Jimmy on 15/12/8.
+//  Copyright © 2015年 Jimmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
